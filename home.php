82f2f65287433dc52bbfb20f3a8b55e8c31ee057
Meymey <meymeyheng2021@gmail.com>
2022-12-04 09:48:38 +0700

    add home page

---
+++ home.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus voluptates, modi ullam culpa perferendis voluptatibus sapiente veritatis! Autem, sequi corrupti dicta eaque, quaerat itaque quae iure illo accusamus nisi officiis!</p>
+</body>
+</html>
